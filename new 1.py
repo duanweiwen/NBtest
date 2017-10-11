038d94c70986423cd652a3664630222a7152749b
duanweiwen <2811038897@qq.com>
2017-10-11 18:49:17 +0800

    test

---
+++ new 1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+if __name__ == '__main__':
+    N = len(a)
+    print a
+    for i in range(len(a) / 2):
+        a[i],a[N - i - 1] = a[N - i - 1],a[i]
+    print a
